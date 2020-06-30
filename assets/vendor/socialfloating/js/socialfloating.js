3762d733875eaef719a18f2e3a85bf0c8d3562ad
larrobbal <robb.strike@gmail.com>
2020-06-29 22:22:30 -0500

    initial commit

---
+++ assets/vendor/socialfloating/js/socialfloating.js
@@ -0,0 +1,20 @@
+$.socialfloating({
+  effect: 'slide-on-scroll',
+  icons: 'semanticui',
+  buttons: [
+    [
+      { icon: 'facebook', enabled: true, link: 'https://www.facebook.com/pages/mycompany', color: '#3b5998' },
+      { icon: 'instagram', enabled: true, link: 'https://www.instagram.com/mycompany', color: '#c13584' },
+      { icon: 'linkedin', enabled: true, link: 'https://www.linkedin.com/company/mycompany', color: '#0077b5' }/*,
+      { icon: 'twitter', enabled: true, link: 'https://www.twitter.com/mycompany', color: '#00aced' },
+      { icon: 'github', enabled: true, link: 'https://www.github.com/mycompany', color: '#333333' }*/
+    ],
+    [
+      { icon: 'phone', enabled: true, link: 'tel:+525546029795' },
+      { icon: 'envelope', enabled: true, link: 'mailto:ventas@botesparabasura.com' }
+    ],
+    [
+      { icon: 'file alternate', enabled: true, link: 'assets/docs/lista_precios.pdf' }
+    ]
+  ]
+});
